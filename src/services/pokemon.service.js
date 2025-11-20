ec276bc54ed3f5d4c482827d1e75512c7a80f43d
Arnau Nin <ninbsnss92@gmail.com>
2025-11-20 11:49:08 +0100

    limit=151

--- src/services/pokemon.service.js
+++ src/services/pokemon.service.js
@@ -2,7 +2,7 @@ import simplifyPokemonList from "../utils/transform.js"
 
 const fetchPokemons = async () => {
 
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     const data = await response.json()
 
     return simplifyPokemonList(data.results)
