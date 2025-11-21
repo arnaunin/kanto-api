fec64636687245b6b4caaea9328152e2f18380b4
Arnau Nin <ninbsnss92@gmail.com>
2025-11-21 20:44:08 +0100

    Update database name in query

--- src/controllers/pokedex.controller.js
+++ src/controllers/pokedex.controller.js
@@ -2,7 +2,7 @@ import { db } from '../database/db'
 
 const getAll = async (req, res) => {
     try {
-        const { rows } = await db.query('SELECT * FROM pokemon')
+        const { rows } = await db.query('SELECT * FROM pokedex')
         res.json(rows)
     } catch (error){
         res.status(500).json({ error: 'Error getting Pokémon'})
--- src/routes/pokedex.routes.js
+++ src/routes/pokedex.routes.js
@@ -8,7 +8,7 @@ router.get('/', getAll)
 
 router.get('/:id', async (req, res) => {
     const { id } = req.params
-    const { rows } = await db.query('SELECT * FROM pokemon WHERE id = $1', [id])
+    const { rows } = await db.query('SELECT * FROM pokedex WHERE id = $1', [id])
 
     if (rows.length === 0) {
         return res.status(404).json({ message: "Pokemon not found"})
@@ -18,19 +18,19 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
     const { nombre } = req.body
-    await db.query('INSERT INTO pokemon (nombre, capturado) VALUES ($1, false)', [nombre])
+    await db.query('INSERT INTO pokedex (nombre, capturado) VALUES ($1, false)', [nombre])
     res.status(201).json({ message: "Pokemon created" })
 })
 
 router.put('/:id', async (req, res) => {
     const { id } = req.params
-    await db.query('UPDATE pokemon SET capturado = NOT capturado WHERE id = $1', [id])
+    await db.query('UPDATE pokedex SET capturado = NOT capturado WHERE id = $1', [id])
     res.json({ message: "Pokemon updated" })
 })
 
 router.delete('/:id', async (req, res) => {
     const { id } = req.params
-    const { rowCount } = await db.query('DELETE FROM pokemon WHERE id = $1', [id])
+    const { rowCount } = await db.query('DELETE FROM pokedex WHERE id = $1', [id])
 
     if (rowCount === 0) {
         return res.status(404).json({ message: "Pokemon not found"})
