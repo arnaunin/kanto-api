f13e9ba032cd3acd0e14478ca102f06adb479533
Arnau Nin <ninbsnss92@gmail.com>
2025-11-21 18:57:16 +0100

    Preparing api for CRUD

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import pokemonRoutes from "./src/routes/pokemon.routes.js"
+import pokedexRoutes from "./src/routes/pokedex.routes.js"
 
 const app = express()
 
@@ -11,6 +12,7 @@ app.use(cors({
 app.use(express.json())
 
 app.use("/pokemon", pokemonRoutes)
+app.use("/pokedex", pokedexRoutes)
 
 app.get("/", (req, res) => {
   res.send("Kanto API is running");
