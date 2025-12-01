424c7ad075f3cd0ef3bf536c65cdf8510456b9a4
Arnau Nin <ninbsnss92@gmail.com>
2025-12-01 12:14:13 +0100

    Pruebas

--- src/services/images.services.js
+++ src/services/images.services.js
@@ -1,6 +1,6 @@
 import { db } from "../database/db.js"
 
 export const getImagesService = async () => {
-    const result = await db.query('SELECT id, nombre, image_url FROM pokedex ORDER BY id')
-    return result
+    const [rows] = await db.query('SELECT id, nombre, image_url FROM pokedex ORDER BY id')
+    return rows
 }
